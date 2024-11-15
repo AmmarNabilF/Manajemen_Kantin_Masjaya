219d4ea222b32fc737cf8fc3a6aca98f7ce930f1
Sabitz <143915759+SabiTzzz@users.noreply.github.com>
2024-11-15 20:22:31 +0800

    mengurangi color

--- tinggal error handling/PA99.cpp
+++ tinggal error handling/PA99.cpp
@@ -12,22 +12,9 @@
 using namespace std;
 
 enum ConsoleColor {
-    BLACK = 0,
-    BLUE = 1,
-    GREEN = 2,
-    CYAN = 3,
     RED = 4,
-    MAGENTA = 5,
-    BROWN = 6,
     LIGHTGRAY = 7,
-    DARKGRAY = 8,
-    LIGHTBLUE = 9,
     LIGHTGREEN = 10,
-    LIGHTCYAN = 11,
-    LIGHTRED = 12,
-    LIGHTMAGENTA = 13,
-    YELLOW = 14,
-    WHITE = 15
 };
 
 // Fungsi Untuk Mengatur Warna Konsol
